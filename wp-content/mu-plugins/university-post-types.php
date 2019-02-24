0036e39e7f53206d5170436d71932fc4c8469752
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2019-02-24 10:51:50 +0530

    Events plugin

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,21 @@
+<?php 
+
+//custom post type  args  'post_type' ie. 'event' & array
+function university_post_type(){
+    register_post_type('event',array(
+        'public'=>true,
+        'menu_icon' =>'dashicons-calendar',
+        'labels'=>array(
+            'name'=>'Events', //name of post type in dashboard
+            'add_new_item'=> 'Add New Events',
+            'edit_item' => 'Ediy Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+            )
+    ));
+}
+
+add_action('init','university_post_type');
+//don't forget 
+//dashboard > settings > permalinks > save changes
+?>
